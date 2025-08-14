467090a8386d9ded6a5dfc02f304deaf8599ba93
Muthulakshmi R <124235201+Muthulakshmi-R@users.noreply.github.com>
2025-08-14 14:17:06 +0530

    Updated Branch and Bound + Heuristics.c

--- Branch and Bound + Heuristics.c
+++ Branch and Bound + Heuristics.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <limits.h>
+
 #define MAX 10
 
 int graph[MAX][MAX];
+int n;
 int visited[MAX];
-int n, best_cost = INT_MAX;
 int path[MAX], best_path[MAX];
-int history[MAX * MAX], histCount = 0;
+int best_cost = INT_MAX;
 
-void addToHistory(int node) {
-    history[histCount++] = node;
-}
-
-void copyPath(int len) {
-    for (int i = 0; i < len; i++) {
-        best_path[i] = path[i];
+int minEdge(int node) {
+    int min = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        if (graph[node][i] != 0 && graph[node][i] < min)
+            min = graph[node][i];
     }
+    return min;
 }
 
-void branchAndBound(int curr_node, int level, int cost) {
-    addToHistory(curr_node);
-    if (cost >= best_cost) return; 
+void branchAndBoundHeuristic(int curr_node, int level, int cost) {
+    int bound = cost;
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) bound += minEdge(i);
+    }
+    if (bound >= best_cost) return;
     if (level == n) {
         if (graph[curr_node][0] != 0 && cost + graph[curr_node][0] < best_cost) {
             best_cost = cost + graph[curr_node][0];
-            path[level] = 0;
-            copyPath(level + 1);
+            for (int i = 0; i < n; i++) best_path[i] = path[i];
+            best_path[n] = 0;
         }
         return;
     }
@@ -33,7 +36,7 @@ void branchAndBound(int curr_node, int level, int cost) {
         if (!visited[i] && graph[curr_node][i] != 0) {
             visited[i] = 1;
             path[level] = i;
-            branchAndBound(i, level + 1, cost + graph[curr_node][i]);
+            branchAndBoundHeuristic(i, level + 1, cost + graph[curr_node][i]);
             visited[i] = 0;
         }
     }
@@ -43,24 +46,17 @@ int main() {
     printf("Enter number of nodes: ");
     scanf("%d", &n);
     printf("Enter adjacency matrix (0 if no edge):\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
             scanf("%d", &graph[i][j]);
-        }
-    }
     for (int i = 0; i < n; i++) visited[i] = 0;
     visited[0] = 1;
     path[0] = 0;
-    branchAndBound(0, 1, 0);
+    branchAndBoundHeuristic(0, 1, 0);
     printf("\nMinimum cost: %d\n", best_cost);
     printf("Optimal path: ");
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i <= n; i++)
         printf("%d ", best_path[i]);
-    }
-    printf("\n\nHistory of all nodes visited: ");
-    for (int i = 0; i < histCount; i++) {
-        printf("%d ", history[i]);
-    }
     printf("\n");
     return 0;
 }
